df4b4e466532e93462933ccc7cc3873ee98ee45e
menna <151149305+mennaabduh@users.noreply.github.com>
2025-01-04 10:30:51 +0200

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6SPAO6BHaMF":
+        Script1();
+        break;
+      case "6pRVIACoT12":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
